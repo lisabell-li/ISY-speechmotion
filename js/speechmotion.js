61739bfbb17b6a2ffe2ab24e5ec15eec589daa4f
LisaJellyBelly <s60392@beuth-hochschule.de>
2016-06-28 00:51:17 +0200

    Added roation for particle box

--- js/speechmotion.js
+++ js/speechmotion.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", function(){
+    var state =1;
     //Babylon engine creation
     var canvas = document.getElementById('gameCanvas');
     var engine = new BABYLON.Engine(canvas, true);
@@ -9,7 +10,7 @@ window.addEventListener("DOMContentLoaded", function(){
     scene.ambientColor= BABYLON.Color3.Red();
 
     //the boxcube
-    var cube = BABYLON.Mesh.CreateBox("cube", 4, scene);
+    var cube = BABYLON.Mesh.CreateBox("cube", 5, scene);
 
     //Free Camera
     var camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(250,200,50), scene);
@@ -17,7 +18,7 @@ window.addEventListener("DOMContentLoaded", function(){
 
     //the ambient light
     var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0,1,0), scene);
-
+    var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(-15, 1, -4), scene);
 
 
     // Create a particle system
@@ -36,8 +37,8 @@ window.addEventListener("DOMContentLoaded", function(){
     particleSystem.colorDead = new BABYLON.Color4(0, 0, 0.2, 0.0);
 
     // Size of each particle (random between...
-    particleSystem.minSize = 0.1;
-    particleSystem.maxSize = 0.5;
+    particleSystem.minSize = 0.4;
+    particleSystem.maxSize = 1.5;
 
     // Life time of each particle (random between...
     particleSystem.minLifeTime = 0.3;
@@ -69,33 +70,45 @@ window.addEventListener("DOMContentLoaded", function(){
     particleSystem.start();
 
 
+    var addRotationAnimation = function( handType, rollRadian) {
+        var degree = rollRadian * (180 / Math.PI);
 
-    // Fountain's animation
-    var keys = [];
-    var animation = new BABYLON.Animation("animation", "rotation.x", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-        BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
-    // At the animation key 0, the value of scaling is "1"
-    keys.push({
-        frame: 0,
-        value: 0
-    });
+        var rotationDegree;
 
-    // At the animation key 50, the value of scaling is "0.2"
-    keys.push({
-        frame: 50,
-        value: Math.PI
-    });
+        if(handType === "left")						// hand roll is from 0 to 180deg
+            rotationDegree= -degree-180;		// back card should roll from -180 to 0
+        else
+            rotationDegree =180-degree;	// hand roll is from 0 to -180deg
 
-    // At the animation key 100, the value of scaling is "1"
-    keys.push({
-        frame: 100,
-        value: 0
-    });
 
-    // Launch animation
-    animation.setKeys(keys);
-    cube.animations.push(animation);
-    scene.beginAnimation(cube, 0, 100, true);
+        // Fountain's animation
+        var keys = [];
+        var animation = new BABYLON.Animation("animation", "rotation.x",  rotationDegree, BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+            BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+        // At the animation key 0, the value of scaling is "1"
+        keys.push({
+            frame: 0,
+            value: 0
+        });
+
+        // At the animation key 50, the value of scaling is "0.2"
+        keys.push({
+            frame: 50,
+            value: Math.PI
+        });
+
+        // At the animation key 100, the value of scaling is "1"
+        keys.push({
+            frame: 100,
+            value: 0
+        });
+
+        // Launch animation
+        animation.setKeys(keys);
+        cube.animations.push(animation);
+        scene.beginAnimation(cube, 0, 100, true);
+    }
+
 
 
 
@@ -120,13 +133,22 @@ window.addEventListener("DOMContentLoaded", function(){
 
     var addGround = function () {
         // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-        var ground = BABYLON.Mesh.CreateGround("ground1", 32, 32, 2, scene);
-        ground.position.y -= 14;
+        var ground = BABYLON.Mesh.CreateGround("ground1", 100, 100,3, scene);
+        ground.material = new BABYLON.StandardMaterial("materialGround", scene);
+        ground.material.backFaceCulling = false;
+        ground.material.diffuseColor = new BABYLON.Color3(0.4, 0.3, 1);
+        ground.position.y -= 10;
     };
 
     var removeGround = function () {
         // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
         scene.getMeshByName("ground1").dispose();
+    };
+    var rotate = function () {
+       state= 2;
+    };
+    var rotateStop = function () {
+        state= 1;
     };
 
 
@@ -143,6 +165,8 @@ window.addEventListener("DOMContentLoaded", function(){
         var commands = {
             'ground': addGround,
             'remove' : removeGround,
+            'rotate'  : rotate,
+            'stop rotate'  : rotateStop,
         };
         annyang.debug(true);
         // Add our commands to annyang
@@ -155,21 +179,51 @@ window.addEventListener("DOMContentLoaded", function(){
 
     // ------------------------------
 
-
+    // -------- Leap Motion ------------
     Leap.loop(function(frame) {
-        if (frame.pointables.length > 0) {
-            var positionLeap = frame.pointables[0].stabilizedTipPosition;
-            var normalized = frame.interactionBox.normalizePoint(positionLeap);
 
-            cube.position.x = positionLeap[2];
-            cube.position.y = positionLeap[1];
-            cube.position.z = positionLeap[0];
-            console.log("x:  "+positionLeap[2] + "  y:  "+positionLeap[1]+"  z:  "+positionLeap[0]);
+        switch(state) {
+            case 1:
+                if (frame.pointables.length > 0) {
+                    var positionLeap = frame.pointables[0].stabilizedTipPosition;
+                    var normalized = frame.interactionBox.normalizePoint(positionLeap);
+
+                    cube.position.x = positionLeap[2];
+                    cube.position.y = positionLeap[1];
+                    cube.position.z = positionLeap[0];
+                    // console.log("x:  "+positionLeap[2] + "  y:  "+positionLeap[1]+"  z:  "+positionLeap[0]);
+                }
+                break;
+            case 2:
+                if(frame.hands.length > 0) {
+                   // addRotationAnimation( frame.hands[0].type, frame.hands[0].roll());
+
+                    var degree = frame.hands[0].roll() * (180 / Math.PI);
+                    if(frame.hands[0].type === "left")						// hand roll is from 0 to 180deg
+                       degree= -degree;
+                    else
+                        degree= degree;
+
+
+                    cube.rotation.x = -frame.hands[0].roll();
+
+                }
+                break;
+            default:
         }
+
+
     });
 
     //the render loop
     engine.runRenderLoop(function(){
         scene.render();
     });
+
+
+
+
+
+
+
 }, false)
